df9f074d776585aa3c61f947650bb68e5a617e9d
Josh Joll <40342278+joshjoll@users.noreply.github.com>
2023-06-17 19:17:30 -0500

    refactor no_books_found helper to work with other endpoints

--- server.rb
+++ server.rb
@@ -11,10 +11,8 @@ end
 namespace '/api/v1' do
 
     before do
-        puts params
         content_type 'application/json'
         @library = Library.new
-
     end
 
     helpers do
@@ -22,8 +20,8 @@ namespace '/api/v1' do
             @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
         end
 
-        def no_books_found
-            halt(404, {message: 'Book not Found'}.to_json)
+        def not_found(subject)
+            halt(404, {message: "#{subject} not Found"}.to_json)
         end
     end
 
@@ -31,14 +29,14 @@ namespace '/api/v1' do
         [:isbn, :author, :title, :genre].each do |filter|
             @library.send(filter.to_sym, params[filter]) if params[filter]
         end
-        no_books_found if @library.books.empty?
+        not_found('Book') if @library.books.empty?
         return @library.books.map{|book| book.serialize(base_url)}.to_json
     end
 
     get '/book/:isbn' do
         @library.isbn(params['isbn'])
         book = @library.books.first
-        no_books_found unless book
+        not_found('Book') unless book
         return book.serialize(base_url).to_json
     end
 end
